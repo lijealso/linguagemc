e64eb3a3ca69ec24e9b2dee5d0f21e71384a92c6
lijealso <lijealso@gmail.com>
2022-07-10 00:37:22 +0100

    3 lines

---
+++ prog0104.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+    printf("C\n");
+    printf("is the greatest programming\n");
+    printf("language\n");
+
+    // single line
+    printf("C\nis the greatest programming\nlanguage\n");
+}
