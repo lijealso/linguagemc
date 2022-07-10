c19362e6c5839830a20b7de2c8d469ffadac00ca
lijealso <lijealso@gmail.com>
2022-07-10 11:27:57 +0100

    exercise, escape characters

---
+++ capitulo1/prog0108.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+
+    printf("Escape characters:\n");
+    printf("\\n\t - \tNewline\n");
+    printf("\\t\t - \tTab\n");
+    printf("\\\\\t - \tBackslash\n");
+    printf("%%%%\t - \tPercentage\n");
+
+}
