52a014c3995feb1692c37272d8dd318db108b1ab
lijealso <lijealso@gmail.com>
2022-07-11 12:59:41 +0100

    exercise 18

---
+++ capitulo1/ex18.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(){
+    /* puts writes every character from the null-terminated string str
+       and one additional newline character '\n' to the output stream stdout */
+    puts("Hello World");
+}
