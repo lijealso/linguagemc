b7e9ec42c30badc3247d2d681e68689858664b8c
lijealso <lijealso@gmail.com>
2022-07-11 12:59:29 +0100

    exercise 17

---
+++ capitulo1/ex17.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("Total\t=\t100%%\n");
+    printf("IVA\t\t=\t100%%\n");
+    printf("IRS\t\t=\t15%%\n");
+    printf("--------------------\n");
+    printf("Liq.\t=\t68%%\n");
+}
